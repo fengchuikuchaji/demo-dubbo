d39289bc6f75d512d1ed9e2b92fb8f6d49547289
leevi <leevi@atguigu.com>
2023-02-14 15:09:27 +0800

    项目初始化

---
+++ api/src/main/java/com/atguigu/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.atguigu.service;
+
+/**
+ * 包名:com.atguigu.service
+ *
+ * @author Leevi
+ * 日期2023-02-05  14:08
+ */
+public interface HelloService {
+    public String sayHello(String name);
+}
---
+++ dubbo-consumer/src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.atguigu.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.service.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 包名:com.atguigu.controller
+ *
+ * @author Leevi
+ * 日期2023-02-05  14:29
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @Reference(loadbalance = "consistenthash")
+    private HelloService helloService;
+
+    @GetMapping("/sayHello")
+    public String sayHello(@RequestParam("name") String name){
+        return helloService.sayHello(name);
+    }
+}
